dda30d80f1975d42a4b0800327f2c512139eaadc
justinmueller05082000 <Justin.Mueller@netways.de>
2019-11-28 13:16:53 +0100

    Changes printing of checkScheduler() into one line
    fixes #2

--- checkScheduler.go
+++ checkScheduler.go
@@ -7,7 +7,7 @@ import (
 
 func checkScheduler() {
 	for true {
-		time.Sleep(10 * time.Second)
+		time.Sleep(30 * time.Second)
 		for x := 1; x <= 100; x++ {
 			fmt.Printf("%s check_disk ", time.Now().Format("Monday 15:04:05 01-02-2006"))
 		}
