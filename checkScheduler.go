fd8192ba3c40314c7cc22de553c03f2346f5f2c9
justinmueller05082000 <Justin.Mueller@netways.de>
2019-11-29 13:39:06 +0100

    Make different breaks for associated checks possible
    fixes #5

--- checkScheduler.go
+++ checkScheduler.go
@@ -8,12 +8,6 @@ import (
 	"time"
 )
 
-type toPrint struct {
-	Text  string  `json:"text"`  // Defines the check's name
-	Times int     `json:"times"` // Defines the repeat of checks
-	Pause float64 `json:"pause"` // Defines the break between the checks
-}
-
 func checkScheduler() {
 	jsonName := flag.String("config", "", "Choose .json file to read the configuration from")
 	flag.Parse()
@@ -24,7 +18,7 @@ func checkScheduler() {
 		return
 	}
 
-	var prints toPrint
+	prints := make(map[string]float64)
 
 	err2 := json.Unmarshal(nameContent, &prints)
 	if err2 != nil {
@@ -32,10 +26,18 @@ func checkScheduler() {
 		return
 	}
 
-	for true {
-		time.Sleep(time.Duration(prints.Pause) * time.Second)
-		for x := 1; x <= prints.Times+1; x++ {
-			fmt.Printf("%s %s \n", time.Now().Format("Monday 15:04:05 01-02-2006"), prints.Text)
-		}
+	for text, pause := range prints {
+		go printTest(pause, text)
+	}
+
+	for {
+		time.Sleep(time.Second)
+	}
+}
+
+func printTest(pause float64, text string) {
+	for {
+		time.Sleep(time.Duration(pause * float64(time.Second)))
+		fmt.Printf("%s %s \n", time.Now().Format("Monday 15:04:05 01-02-2006"), text)
 	}
 }
