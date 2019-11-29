bc712fa7e83462c6167126852c38ad00148c7c06
justinmueller05082000 <Justin.Mueller@netways.de>
2019-11-29 10:18:28 +0100

    Make config's root an object
    fixes #3

--- checkScheduler.go
+++ checkScheduler.go
@@ -13,31 +13,27 @@ type toPrint struct {
 }
 
 func checkScheduler() {
-	jsonname := flag.String("config", "", "Configuration of the output message")
+	jsonname := flag.String("config", "", "Choose .json file to read the configuration from")
 	flag.Parse()
 
 	content, err := ioutil.ReadFile(*jsonname)
 	if err != nil {
 		fmt.Println(err.Error())
+		return
 	}
 
-	var prints []toPrint
+	var prints toPrint
 
 	err2 := json.Unmarshal(content, &prints)
 	if err2 != nil {
 		fmt.Println(err2.Error())
-	}
-
-	var text string
-
-	for _, x := range prints {
-		text = x.Text
+		return
 	}
 
 	for true {
 		time.Sleep(30 * time.Second)
 		for x := 1; x <= 100; x++ {
-			fmt.Printf("%s %s \n", time.Now().Format("Monday 15:04:05 01-02-2006"), text)
+			fmt.Printf("%s %s \n", time.Now().Format("Monday 15:04:05 01-02-2006"), prints.Text)
 		}
 	}
 }
