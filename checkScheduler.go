740a5bf47002dc986cae12703ad4befee2969d31
Justin Müller <Justin.Mueller@netways.de>
2019-12-06 13:34:28 +0100

    Replace parallel printing
    refs #6

--- checkScheduler.go
+++ checkScheduler.go
@@ -26,14 +26,26 @@ func checkScheduler() {
 		return
 	}
 
-	i := 0
+	timeAbsolute := make(map[string]time.Time)
+
+	for service, value := range prints {
+		timeAbsolute[service] = time.Now().Add(time.Duration(float64(time.Second) * value))
+	}
 
 	for {
-		time.Sleep(time.Second)
-		i++
-		for text, pause := range prints {
-			if i%int(pause) == 0 {
+		lowestValue := time.Unix(1<<63-1, 1<<63-1)
+
+		for _, value := range timeAbsolute {
+			if value.Before(lowestValue) {
+				lowestValue = value
+			}
+		}
+
+		time.Sleep(lowestValue.Sub(time.Now()))
+		for text, value := range timeAbsolute {
+			if !time.Now().Before(value) {
 				fmt.Printf("%s %s \n", time.Now().Format("Monday 15:04:05 01-02-2006"), text)
+				timeAbsolute[text] = value.Add(time.Duration(prints[text] * float64(time.Second)))
 			}
 		}
 	}
